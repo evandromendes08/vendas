34e885e892281d8cebbf44ac8a81d422105e41f4
evadromendes08 <evandromende@gmail.com>
2024-05-12 21:41:08 -0300

    conexão com DB

---
+++ src/database/database.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+function connectToDatabase(){
+    mongoose.connect("mongodb://localhost:27017", {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }).then(() => {
+        console.log("MONGO DB CONECTADO");
+    }).catch((err) =>{
+        return console.log(`Erro ao conectar com o Banco de dados: ${err}`);
+    })
+}
+
+module.exports = connectToDatabase;
