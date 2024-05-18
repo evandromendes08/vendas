9309435defd7145974c9c88a201b0764b94e48a3
evadromendes08 <evandromende@gmail.com>
2024-05-17 21:29:12 -0300

    Feate usuario controller

--- src/router/usuario.router.js
+++ src/router/usuario.router.js
@@ -1,18 +1,19 @@
 const express = require("express");
 const router = express.Router();
 const usuarioController = require("../controller/usuario.controller");
+const { removeUserAddressController, removeUserFavProduct } = require("../controller/database/usuario.controller");
 
-router.get('/findById:id');
-router.get('/findAll');
+router.get('/findById:id', usuarioController.findUserByController);
+router.get('/findAll', usuarioController.findAllUserController);
 
-router.post('/create');
-router.post('addAdress/:id');
-router.post('/addFavProduct/:id');
+router.post('/create', usuarioController.createUserController);
+router.post('addAdress/:id', usuarioController.addUserAddressController);
+router.post('/addFavProduct/:id',usuarioController.addUserFavController);
 
-router.put('/update/:id');
+router.put('/update/:id', usuarioController.updateUserController);
 
-router.delete('/remouve/:id');
-router.delete('/removeAdress');
-router.delete('/removeFavProduct');
+router.delete('/remove/:id', usuarioController.removeUserController);
+router.delete('/removeAdress', usuarioController.removeUserAddressController);
+router.delete('/removeFavProduct', usuarioController.removeUserFavProduct);
 
 module.exports = router;
