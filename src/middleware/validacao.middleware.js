2f6a839281b6631970794282d96ca1a51f6d445c
evadromendes08 <evandromende@gmail.com>
2024-05-19 14:23:26 -0300

    Feat: add de validacoes

--- src/middleware/validacao.middleware.js
+++ src/middleware/validacao.middleware.js
@@ -1,51 +1,94 @@
 
 const validaUsuario = (req, res, next) => {
+    //testa um erro de cada vez
     if(!req.body.nome){
-        return res.status(400).send({ message: "O campo nome deve ser preenchido"});
+        return res.status(400).send({ message: `O campo 'nome' deve ser preenchido!`});
     }
     if(!req.body.email){
-        return res.status(400).send({ message: "O campo email deve ser preenchido"});
+        return res.status(400).send({ message: `O campo 'email' deve ser preenchido!`});
     }
     if(!req.body.senha){
-        return res.status(400).send({ message: "O campo senha deve ser preenchido"});
+        return res.status(400).send({ message: `O campo 'senha' deve ser preenchido!`});
     }
     if(!req.body.imagem){
-        return res.status(400).send({ message: "O campo imagem deve ser preenchido"});
+        return res.status(400).send({ message: `O campo 'imagem' deve ser preenchido!`});
+    }
+    if(!req.body.admin){
+        return res.status(400).send({ message: `O campo 'admin' deve ser preenchido!`});
     }
     return next();
 } 
 
+const validaProduto = (req, res, next) => {
+    let erros = []; //variavel para acumular erros
+    if(!req.body.nome){
+        erros.push("nome");
+    }
+    if(!req.body.descricao){
+        erros.push("descricao");
+    }
+    if(!req.body.precoUnitario){
+        erros.push("precoUnitario");
+    }
+    if(!req.body.imagem){
+        erros.push("imagem");
+    }
+    if(!req.body.codigoBarra){
+        erros.push("codigoBarra");
+    }
+        //     return res.status(400).send({ message: `O campo 'nome' deve ser preenchido`});
+        // }
+    // if(!req.body.nome){
+    //     return res.status(400).send({ message: `O campo 'nome' deve ser preenchido`});
+    // }
+    // if(!req.body.descricao){
+    //     return res.status(400).send({ message: `O campo 'descricao' deve ser preenchido`});
+    // }
+    // if(!req.body.precoUnitario){
+    //     return res.status(400).send({ message: `O campo 'precoUni' deve ser preenchido`});
+    // }
+    // if(!req.body.imagem){
+    //     return res.status(400).send({ message: `O campo 'imagem' deve ser preenchido`});
+    // }
+    // if(!req.body.codigoBarra){
+    //     return res.status(400).send({ message: "O campo 'codigoBarra' deve ser preenchido"});
+    // }
+
+    //testando quantos erros temos e verificar o que fazer
+    if(erros.length == 0){
+        return next();
+    }else{
+        if(erros.length > 1){
+            return res.status(400).send({ message: `Os campos ${erros} devem ser preenchidos!`}); 
+        }else{
+        return res.status(400).send({ message: `O campo ${erros} deve ser preenchido!`}); 
+        }
+    }
+    
+}
+
+const validaCategoria = (req, res, next) => {
+    
+    if(!req.body.nome){
+        return res.status(400).send({ message: `O campo 'nome' deve ser preenchido`});
+    }
+
+    return next();
+}
+
 const validaEndereco = (req, res, next) => {
     if(!req.body.rua){
-        return res.status(400).send({ message: "O campo rua deve ser preenchido"});
+        return res.status(400).send({ message: `O campo 'rua' deve ser preenchido`});
     }
     if(!req.body.numero){
-        return res.status(400).send({ message: "O campo numero deve ser preenchido"});
+        return res.status(400).send({ message: `O campo 'numero' deve ser preenchido`});
     }
     if(!req.body.CEP){
-        return res.status(400).send({ message: "O campo CEP deve ser preenchido"});
+        return res.status(400).send({ message: `O campo 'CEP' deve ser preenchido`});
     }
     return next();
 }
 
-const validaProduto = (req, res, next) => {
-    if(!req.body.nome){
-        return res.status(400).send({ message: "O campo nome deve ser preenchido"});
-    }
-    if(!req.body.descricao){
-        return res.status(400).send({ message: "O campo descricao deve ser preenchido"});
-    }
-    if(!req.body.precoUni){
-        return res.status(400).send({ message: "O campo precoUni deve ser preenchido"});
-    }
-    if(!req.body.imagem){
-        return res.status(400).send({ message: "O campo imagem deve ser preenchido"});
-    }
-    if(!req.body.codigoBarra){
-        return res.status(400).send({ message: "O campo codigoBarra deve ser preenchido"});
-    }
-    return next();
-}
 
 const validaPedido = (req, res, next) => {
     let campos = "";
@@ -59,14 +102,6 @@ const validaPedido = (req, res, next) => {
 
     if(campos != ""){
         return res.status(400).send({message: `O(s) campo(s) ${campos}, deve(m) ser preenchido(s)`})
-    }
-
-    return next();
-}
-const validaCategoria = (req, res, next) => {
-    
-    if(!req.body.nome){
-        return res.status(400).send({ message: "O campo nome deve ser preenchido"});
     }
 
     return next();
@@ -85,8 +120,8 @@ const validaCarrinho = (req, res, next) => {
 
 module.exports = {
     validaUsuario,
-    validaEndereco,
     validaProduto,
+    validaEndereco,    
     validaPedido,
     validaCategoria,
     validaCarrinho
--- src/router/usuario.router.js
+++ src/router/usuario.router.js
@@ -4,23 +4,24 @@ const usuarioController = require("../controller/usuario.controller");
 const { removeUserAddressController, removeUserFavProductController } = require("../controller/usuario.controller");
 
 const authMiddleware = require("../middleware/auth.middleware");
+const {validaUsuario} = require("../middleware/validacao.middleware");
 
 //rotas get
 router.get('/findById/:id',authMiddleware, usuarioController.findUserByIdController);
 router.get('/findAll', authMiddleware, usuarioController.findAllUsersController);
 
 //rotas post
-router.post('/create', usuarioController.createUserController);
-router.post('addAdress/:id', usuarioController.addUserAddressController);
-router.post('/addFavProduct/:id',usuarioController.addUserFavProductController);
+router.post('/create', validaUsuario,usuarioController.createUserController);
+router.post('addAdress/:id', authMiddleware,usuarioController.addUserAddressController);
+router.post('/addFavProduct/:id', authMiddleware,usuarioController.addUserFavProductController);
 
 //rotas put
-router.put('/update/:id', usuarioController.updateUserController);
+router.put('/update/:id', authMiddleware, validaUsuario,usuarioController.updateUserController);
 
 //rotas delete
-router.delete('/remove/:id', usuarioController.removeUserController);
-router.delete('/removeAdress', usuarioController.removeUserAddressController);
-router.delete('/removeFavProduct/:id', usuarioController.removeUserFavProductController);
+router.delete('/remove/:id', authMiddleware, usuarioController.removeUserController);
+router.delete('/removeAdress', authMiddleware, usuarioController.removeUserAddressController);
+router.delete('/removeFavProduct/:id', authMiddleware, usuarioController.removeUserFavProductController);
 
 module.exports = {
     router,
